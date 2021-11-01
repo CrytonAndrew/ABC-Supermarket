302620b1a035cc5260bbf5ca75a81f28678d7e3b
Cryton Andrew <crytonadrew21@gmail.com>
2021-11-01 16:03:35 +0200

    Upload image route

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import connectDB from "./config/db.js"
 
 import itemRoutes from './routes/itemRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import uploadRoutes from "./routes/uploadRoutes.js"
 
 dotenv.config()
 
@@ -23,10 +24,22 @@ app.use(express.json())
 
 app.use('/api/items', itemRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/upload', uploadRoutes)
 
-app.get("/", (req, res) => {
-    res.send("API is running..... ")
-})
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
